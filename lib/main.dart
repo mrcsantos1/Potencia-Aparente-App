5aac1491a7f8adf0d776c4915e9742138b1552b9
Marcos A Santos <mrcsantos1@outlook.com>
2020-04-26 03:08:28 -0300

    App pronto - versao 1.0 - OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'dart:math';
-
+import 'package:flutter/widgets.dart';
+import 'package:validators/validators.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.Dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.black, // navigation bar color
+    statusBarColor: Colors.black, // status bar color
+    statusBarBrightness: Brightness.dark,
+  ));
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -15,6 +22,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   TextEditingController tensaoController = TextEditingController();
   TextEditingController correnteController = TextEditingController();
   TextEditingController fatorPotenciaController = TextEditingController();
@@ -27,13 +36,16 @@ class _HomeState extends State<Home> {
   bool _indutivo = false;
 
   void _resetFields() {
-    tensaoController.text = "";
-    correnteController.text = "";
-    fatorPotenciaController.text = "";
-    _infoText1 = "Informe os dados";
-    _infoText2 = "";
-    _infoText3 = "";
-    _infoText4 = "";
+    setState(() {
+      tensaoController.text = "";
+      correnteController.text = "";
+      fatorPotenciaController.text = "";
+      _infoText1 = "Informe os dados";
+      _infoText2 = "";
+      _infoText3 = "";
+      _infoText4 = "";
+      _formKey = GlobalKey<FormState>();
+    });
   }
 
   void _calculate() {
@@ -48,17 +60,20 @@ class _HomeState extends State<Home> {
       double p = smod * fp;
 
       double q = smod * sin(fp);
-
+      ang = -ang * 180 / pi;
       if (_indutivo) {
-        _infoText1 = "S polar = $smod, ${-ang * 180 / pi}°\tVA";
-        _infoText2 = "P = $p\tW";
-        _infoText3 = "Q = -$q\tVAr";
-        _infoText4 = "S ret = $p - j$q";
+        _infoText1 =
+            "S polar = ${smod.toStringAsPrecision(4)}, ${ang.toStringAsPrecision(4)}°\tVA";
+        _infoText2 = "P = ${p.toStringAsPrecision(4)}\tW";
+        _infoText3 = "Q = -${q.toStringAsPrecision(4)}\tVAr";
+        _infoText4 =
+            "S ret = ${p.toStringAsPrecision(4)} - j${q.toStringAsPrecision(4)}";
       } else if (!_indutivo) {
-        _infoText1 = "S polar = $smod, ${ang * 180 / pi}°\tVA";
-        _infoText2 = "P = $p\tW";
-        _infoText3 = "Q = $q\tVAr";
-        _infoText4 = "S ret = $p + j$q";
+        _infoText1 = "S polar = $smod, ${ang.toStringAsPrecision(4)}°\tVA";
+        _infoText2 = "P = ${p.toStringAsPrecision(4)}\tW";
+        _infoText3 = "Q = ${q.toStringAsPrecision(4)}\tVAr";
+        _infoText4 =
+            "S ret = ${p.toStringAsPrecision(4)} + j${q.toStringAsPrecision(4)}";
       }
     });
   }
@@ -67,7 +82,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Calcula Potência Aparente"),
+          title: Text("Calcula Potência Aparente \tBy MrcSantos"),
           centerTitle: true,
           backgroundColor: Colors.black,
           actions: <Widget>[
@@ -77,130 +92,178 @@ class _HomeState extends State<Home> {
             )
           ],
         ),
-        backgroundColor: Colors.white30,
+        backgroundColor: Colors.white60,
         body: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(
-                Icons.power,
-                size: 200.0,
-                color: Colors.black,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                    labelText: "Módulo da tensão (V): ",
-                    labelStyle: TextStyle(color: Colors.black, fontSize: 30.0)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 20.0),
-                controller: tensaoController,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                    labelText: "Módulo da corrente (A): ",
-                    labelStyle: TextStyle(color: Colors.black, fontSize: 30.0)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 20.0),
-                controller: correnteController,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                    labelText: "Fator de Potência",
-                    labelStyle: TextStyle(color: Colors.black, fontSize: 30.0)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 20.0),
-                controller: fatorPotenciaController,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Text(
-                    "Indutivo",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 20.0),
+                  Icon(
+                    Icons.power,
+                    size: 200.0,
+                    color: Colors.black,
                   ),
-                  Switch(
-                    value: _indutivo,
-                    onChanged: (value) {
-                      setState(() {
-                        _indutivo = value;
-                      });
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      labelText: "Módulo da tensão (V): ",
+                      labelStyle:
+                          TextStyle(color: Colors.black, fontSize: 30.0),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.white, style: BorderStyle.solid),
+                      ),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    controller: tensaoController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira um valor para tensão!";
+                      }
+                      if (!isFloat(value)) {
+                        return "Digite apenas números!";
+                      }
                     },
-                    inactiveThumbColor: Colors.white,
-                    inactiveTrackColor: Colors.black,
-                    activeTrackColor: Colors.white,
-                    activeColor: Colors.black,
                   ),
-                  Text(
-                    "Capacitivo",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 20.0),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        labelText: "Módulo da corrente (A): ",
+                        labelStyle:
+                            TextStyle(color: Colors.black, fontSize: 30.0),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.white, style: BorderStyle.solid),
+                        )),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    controller: correnteController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira um valor para tensão!";
+                      }
+                      if (!isFloat(value)) {
+                        return "Digite apenas números!";
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        labelText: "Fator de Potência",
+                        labelStyle:
+                            TextStyle(color: Colors.black, fontSize: 30.0),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.white, style: BorderStyle.solid),
+                        )),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    controller: fatorPotenciaController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira um valor para tensão!";
+                      }
+                      if (!isFloat(value)) {
+                        return "Digite apenas números!";
+                        // ignore: missing_return, missing_return, missing_return, missing_return
+                      }
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Indutivo",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 20.0),
+                      ),
+                      Switch(
+                        value: _indutivo,
+                        onChanged: (value) {
+                          setState(() {
+                            _indutivo = value;
+                          });
+                        },
+                        inactiveThumbColor: Colors.white,
+                        inactiveTrackColor: Colors.black,
+                        activeTrackColor: Colors.white,
+                        activeColor: Colors.black,
+                      ),
+                      Text(
+                        "Capacitivo",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 20.0),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15.0, bottom: 30.0),
+                    child: Container(
+                        height: 70.0,
+                        child: RaisedButton(
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              _calculate();
+                            }
+                          },
+                          child: Text(
+                            "Calcular!",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 25.0),
+                          ),
+                          color: Colors.black,
+                        )),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Container(
+                        height: 30.0,
+                        child: Text(
+                          _infoText1,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      ),
+                      Container(
+                        height: 30.0,
+                        child: Text(
+                          _infoText2,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      ),
+                      Container(
+                        height: 30.0,
+                        child: Text(
+                          _infoText3,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      ),
+                      Container(
+                        height: 30.0,
+                        child: Text(
+                          _infoText4,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 15.0, bottom: 30.0),
-                child: Container(
-                    height: 70.0,
-                    child: RaisedButton(
-                      onPressed: _calculate,
-                      child: Text(
-                        "Calcular!",
-                        style: TextStyle(color: Colors.white, fontSize: 25.0),
-                      ),
-                      color: Colors.black,
-                    )),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Container(
-                    height: 30.0,
-                    child: Text(
-                      _infoText1,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-                    ),
-                  ),
-                  Container(
-                    height: 30.0,
-                    child: Text(
-                      _infoText2,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-                    ),
-                  ),
-                  Container(
-                    height: 30.0,
-                    child: Text(
-                      _infoText3,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-                    ),
-                  ),
-                  Container(
-                    height: 30.0,
-                    child: Text(
-                      _infoText4,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ));
+            )));
   }
 }
